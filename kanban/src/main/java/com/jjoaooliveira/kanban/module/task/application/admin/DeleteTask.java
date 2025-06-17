45a69d05e1a43be0ec2d87d82232e5388ccacf4f
joao <joaooliv11@hotmail.com>
2025-06-17 19:17:04 -0400

    feat: add delete task use case

---
+++ kanban/src/main/java/com/jjoaooliveira/kanban/module/task/application/admin/DeleteTask.java
@@ -0,0 +1,27 @@
+package com.jjoaooliveira.kanban.module.task.application.admin;
+
+import com.jjoaooliveira.kanban.module.task.application.TaskDataGateway;
+import com.jjoaooliveira.kanban.module.task.application.TaskRequest;
+import com.jjoaooliveira.kanban.module.task.domain.Description;
+import com.jjoaooliveira.kanban.module.task.domain.Task;
+import com.jjoaooliveira.kanban.module.task.domain.Title;
+
+public class DeleteTask {
+    private final TaskDataGateway dataGateway;
+
+    public DeleteTask(TaskDataGateway dataGateway) {
+        this.dataGateway = dataGateway;
+    }
+
+    public void doDelete(TaskRequest request) {
+        Title title = new Title(request.title());
+        Description description = new Description(request.description());
+        Task task = new Task(request.id(), 
+                            title, 
+                            description, 
+                            request.date(), 
+                            request.kanbanId());
+                            
+        dataGateway.deleteTask(task);
+    }
+}
