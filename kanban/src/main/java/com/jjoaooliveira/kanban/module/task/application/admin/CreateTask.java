7babe7982218b05183873eb9d9f354d507bc2535
joao <joaooliv11@hotmail.com>
2025-06-17 19:11:27 -0400

    feat: add create task use case

---
+++ kanban/src/main/java/com/jjoaooliveira/kanban/module/task/application/admin/CreateTask.java
@@ -0,0 +1,29 @@
+package com.jjoaooliveira.kanban.module.task.application.admin;
+
+import com.jjoaooliveira.kanban.module.task.application.TaskDataGateway;
+import com.jjoaooliveira.kanban.module.task.application.TaskRequest;
+import com.jjoaooliveira.kanban.module.task.application.TaskResponse;
+import com.jjoaooliveira.kanban.module.task.domain.Description;
+import com.jjoaooliveira.kanban.module.task.domain.Task;
+import com.jjoaooliveira.kanban.module.task.domain.Title;
+
+public class CreateTask {
+    private final TaskDataGateway dataGateway;
+
+    public CreateTask(TaskDataGateway dataGateway) {
+        this.dataGateway = dataGateway;
+    }
+
+    public TaskResponse doCreate(TaskRequest request) {
+        Title title = new Title(request.title());
+        Description description = new Description(request.description());
+        Task task = new Task(request.id(), 
+                            title,
+                            description, 
+                            request.date(), 
+                            request.kanbanId());
+
+        Task persistedTask = dataGateway.saveTask(task);
+        return TaskResponse.fromEntity(persistedTask);
+    }
+}
